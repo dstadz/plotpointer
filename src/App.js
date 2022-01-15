e039bbe40d364410d02a07d38caef68b54154f92
Drazen <danstad2012@gmail.com>
2022-01-15 10:47:53 -0800

    re og

--- src/App.js
+++ src/App.js
@@ -8,13 +8,85 @@ import 'react-flow-renderer/dist/style.css';
 import 'react-flow-renderer/dist/theme-default.css';
 import OverviewFlow from './TimeLine/overview';
 
+import {useState, useEffect} from 'react'
+import {collection, query, orderBy, onSnapshot} from "firebase/firestore"
+import {db} from './utils/firebase'
+import ReactFlow, { removeElements, addEdge } from 'react-flow-renderer';
+import { TimeLineWrapper } from './styles';
+import { addToFirebase } from './utils/firebase'
+import { useCollectionData, useCollection } from 'react-firebase-hooks/firestore';
+import { useMoveNode } from './utils/hooks/useMoveNode'
+import CustomNodeFlow from './TimeLine/CustomNodeFlow';
+
+
 const App = () => {
+  const [elements, setElements] = useState([]);
+  const { logNode } = useMoveNode()
+  const [nodeValues, nodeLoading, nodeError] = useCollectionData(query(collection(db, 'nodes')))
+  const [edgeValues, edgeLoading, edgeError] = useCollectionData(query(collection(db, 'edges')))
+
+  useEffect(() => {
+    if (nodeError) {console.error(nodeError)}
+    if (edgeError) {console.error(edgeError)}
+    if (nodeValues && edgeValues) {
+      console.log(nodeValues, edgeValues)
+      setElements([...nodeValues, ...edgeValues])
+    }
+  }, [nodeLoading, edgeLoading])
+
+const [value, setValue] = useState('')
 
 
+
+const onElementsRemove = (elementsToRemove) =>{
+setElements((els) => removeElements(elementsToRemove, els));
+}
+
+
+const tickleState = () => {
+console.log(` tickle state:`, elements)
+}
+
+const handleaddToFirebaseFormSubmit = async () => {
+// e.preventDefault()
+  // console.log('handleaddToFirebaseFormSubmit')
+  const newNode = {
+    data: { label: value },
+    type: 'eventNode',
+    position: {
+      x: Math.floor(Math.random()*100)+150,
+      y: Math.floor(Math.random()*100)+150
+    }
+  }
+  const newNodeId = await addToFirebase('nodes', newNode)
+  console.log(newNodeId)
+  setElements((els) =>([...els, {...newNode, id: newNodeId}]))
+}
+
+
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+    }
 
   return <AppWrapper>
-    <TimeLine />
+  <button onClick={tickleState}>tickle Nodes</button>
 
+<form onSubmit={(e)=> {
+e.preventDefault();
+handleaddToFirebaseFormSubmit(value)
+}}>
+<input type="text" value={value} onChange={handleChange} />
+
+<input type="submit"/>
+</form>
+
+    <TimeLine
+      elements={elements}
+      setElements={setElements}
+    />
+
+    {/* <CustomNodeFlow /> */}
   </AppWrapper>
 }
 
--- src/TimeLine/TimeLine.js
+++ src/TimeLine/TimeLine.js
@@ -27,12 +27,11 @@ import {  MiniMap, Controls } from 'react-flow-renderer';
 // }
 
 import { useMoveNode } from '../utils/hooks/useMoveNode'
+import EventNode from './EventNode'
 
-const TimeLine = () => {
-  const [elements, setElements] = useState([]);
+const TimeLine = ({elements, setElements}) => {
   const { logNode } = useMoveNode()
-  const [nodeValues, nodeLoading, nodeError] = useCollectionData(query(collection(db, 'nodes')))
-  const [edgeValues, edgeLoading, edgeError] = useCollectionData(query(collection(db, 'edges')))
+
   const onNodeDragStart = (event, node) => {}//console.log('drag start', node);
   const onNodeDragStop = (event, node) =>{
     // console.log('drag stop', node)
@@ -40,92 +39,49 @@ const TimeLine = () => {
   };
   const onElementClick = (event, element) => console.log('click', element);
 
-  useEffect(() => {
-    if (nodeError) {console.error(nodeError)}
-    if (edgeError) {console.error(edgeError)}
-    if (nodeValues && edgeValues) {setElements([...nodeValues, ...edgeValues])}
-  }, [nodeLoading, edgeLoading])
-
-const [value, setValue] = useState('')
 
 
+  const onConnect = (params) => {
+    console.log("edge parameters:", params)
+    setElements((els) => {
+      console.log('prevlist', els)
+      const edge = addEdge(params, els)
+      console.log(edge[-1], edge)
+
+
+      addToFirebase('edges', edge[edge.length - 1] )
+      return edge
+    })};
+
+    const nodeTypes = {
+      eventNode: EventNode,
+    };
+
+return (
+<TimeLineWrapper id='timeline'>
+  <ReactFlow
+     nodeTypes={nodeTypes}
 
 
 
-const onConnect = (params) => {
-console.log("edge parameters:", params)
-setElements((els) => {
-  console.log('prevlist', els)
-  const edge = addEdge(params, els)
-  console.log(edge[-1], edge)
-
-
-  addToFirebase('edges', edge[edge.length - 1] )
-  return edge
-})};
-
-const onElementsRemove = (elementsToRemove) =>{
-setElements((els) => removeElements(elementsToRemove, els));
-}
-
-
-const tickleState = () => {
-console.log(` tickle state: ${elements}`, elements)
-}
-
-const handleaddToFirebaseFormSubmit = async () => {
-// e.preventDefault()
-  // console.log('handleaddToFirebaseFormSubmit')
-  const newNode = {
-    data: { label: value },
-    position: {
-      x: Math.floor(Math.random()*100)+150,
-      y: Math.floor(Math.random()*100)+150
-    }
-  }
-  const newNodeId = await addToFirebase('nodes', newNode)
-  console.log(newNodeId)
-  setElements((els) =>([...els, {...newNode, id: newNodeId}]))
-}
-
-
-const handleChange = (e) => {
-setValue(e.target.value)
-}
-
-return <>
-<button onClick={tickleState}>tickle Nodes</button>
-
-<form onSubmit={(e)=> {
-e.preventDefault();
-handleaddToFirebaseFormSubmit(value)
-}}>
-<input type="text" value={value} onChange={handleChange} />
-
-<input type="submit"/>
-</form>
-
-<TimeLineWrapper>
-    <ReactFlow
-      elements={elements}
-      elementsSelectable={true}
-      nodesConnectable={true}
-      nodesDraggable={true}
-      zoomOnScroll={true}
-      panOnScroll={true}
-      panOnScrollMode={true}
-      zoomOnDoubleClick={true}
-      onConnect={onConnect}
-      onElementClick={onElementClick}
-      onNodeDragStart={onNodeDragStart}
-      onNodeDragStop={onNodeDragStop}
-      // onLoad={onLoad}
-    >
-      <MiniMap />
-      <Controls />
-    </ReactFlow>
+    elements={elements}
+    elementsSelectable={true}
+    nodesConnectable={true}
+    nodesDraggable={true}
+    zoomOnScroll={true}
+    panOnScroll={true}
+    panOnScrollMode={true}
+    zoomOnDoubleClick={true}
+    onConnect={onConnect}
+    onElementClick={onElementClick}
+    onNodeDragStart={onNodeDragStart}
+    onNodeDragStop={onNodeDragStop}
+    // onLoad={onLoad}
+  >
+    <MiniMap />
+    <Controls />
+  </ReactFlow>
 </TimeLineWrapper>
-</>
-}
+)}
 
 export default TimeLine
