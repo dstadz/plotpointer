c31c7e4f82fbd7858afde3b6c25ba93196dc5777
Drazen <danstad2012@gmail.com>
2022-01-29 20:38:09 -0800

    house keeping

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { AppWrapper } from './styles';
 import TimeLine from './TimeLine/TimeLine';
-import './App.css';
 import 'react-flow-renderer/dist/style.css';
 import 'react-flow-renderer/dist/theme-default.css';
 import Sidebar from './Components/Sidebar';
+import { ReactFlowProvider } from 'react-flow-renderer';
 
 const App = () => {
 
 return <AppWrapper id="app">
-  <Sidebar />
-  <TimeLine />
+  <ReactFlowProvider>
+    <Sidebar />
+    <TimeLine />
+  </ReactFlowProvider>
 </AppWrapper>
 }
 
